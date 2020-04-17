70a6ab13a4eb9d9a8a2dac0be3585f892bb13ede
Jakeokony1024 <jakeokony@gmail.com>
2020-04-17 14:08:42 -0500

    added variables for scores

--- clickygame/src/App.js
+++ clickygame/src/App.js
@@ -1,25 +1,124 @@
 import React, { Component } from 'react';
 import Container from './components/Container'
 import ImageCard from './components/ImageCard'
-import ImgContainer from './components/ImgContainer'
-import Score from './components/Score'
+// import ImgContainer from './components/ImgContainer'
+import Title from './components/Title'
 import friends from './friends.json'
 import './App.css';
+
+let correctGuesses = 0;
+let bestScore = 0;
+let clickMessage = "Click on a friend's image to gain points! Click on the same one twice and you lose!";
 
 class App extends Component{
 
   state = {
-    friends
+    friends,
+    correctGuesses,
+    bestScore,
+    clickMessage
   }
 
+  imageClick = id => {
+    
+    const friends = this.state.friends;
+    const clickedFriend = friends.filter(friend => friend.id === id);
+    
+        if (clickedFriend[0].clicked) {
+
+          console.log ("Correct Guesses: " + correctGuesses);
+          console.log ("Best Score: " + bestScore);
+
+          correctGuesses = 0;
+          clickMessage = "Bummer! You already clicked on this one."
+
+          for (let i = 0 ; i < friends.length ; i++) {
+              friends[i].clicked = false;
+          }
+
+          this.setState({clickMessage});
+          this.setState({ correctGuesses });
+          this.setState({friends});
+        
+      } else if (correctGuesses < 11) {
+
+        // Set its value to true
+        clickedFriend[0].clicked = true;
+
+        // increment the appropriate counter
+        correctGuesses++;
+        
+        clickMessage = "Great! You haven't click on that one yet! Keep going!";
+
+        if (correctGuesses > bestScore) {
+            bestScore = correctGuesses;
+            this.setState({ bestScore });
+        }
+
+        // Shuffle the array to be rendered in a random order
+        friends.sort(function(a, b){return 0.5 - Math.random()});
+
+        // Set this.state.matches equal to the new matches array
+        this.setState({ friends });
+        this.setState({correctGuesses});
+        this.setState({clickMessage});
+    } else {
+
+      // Set its value to true
+      clickedFriend[0].clicked = true;
+
+      // restart the guess counter
+      correctGuesses = 0;
+
+      // Egg on the user to play again
+      clickMessage = "WOW!!! You got ALL of them!!! Now, let's see if you can do it again!";
+      bestScore = 12;
+      this.setState({ bestScore });
+      
+      for (let i = 0 ; i < friends.length ; i++){
+          friends[i].clicked = false;
+      }
+
+      // Shuffle the array to be rendered in a random order
+      friends.sort(function(a, b){return 0.5 - Math.random()});
+
+      // Set this.state.matches equal to the new matches array
+      this.setState({ friends });
+      this.setState({correctGuesses});
+      this.setState({clickMessage});
+
+  }
+
+  }
   render() {
-    return(
+    return (
       <Container>
-        {this.state.friends.map(friend => (
-          <ImageCard id={ friend.id} image={friend.image}/>
-        ))}
+          <Title>Clicky Game</Title>
+  
+          <h3 className="scoreSummary">
+              {this.state.clickMessage}
+          </h3>
+          
+          <h3 className="scoreSummary card-header">
+              Correct Guesses: {this.state.correctGuesses} 
+              <br />
+              Best Score: {this.state.bestScore} 
+          </h3>
+          <div className="container">
+          <div className="row">
+          {this.state.friends.map(friend => (
+              <ImageCard
+                  imageClick={this.imageClick}
+                  id={friend.id}
+                  key={friend.id}
+                  image={friend.image}
+              />
+          ))}
+          </div>
+          </div>
+
       </Container>
-    )
+  );
   }
 }
 // function App() {
--- clickygame/src/components/ImageCard/index.js
+++ clickygame/src/components/ImageCard/index.js
@@ -3,14 +3,18 @@ import './style.css'
 
 function ImageCard(props) {
     return (
-      <div className="card">
-        <div className="img-container">
-          <img alt={props.name} src={props.image} />
-        </div>
-        <span onClick={() => props.removeFriend(props.id)} className="remove">
-          𝘅
-        </span>
-      </div>
+    //   <div className="card">
+    //     <div className="img-container">
+    //     <img alt={props.name} src={props.image} />
+    //     </div>
+    //     <span onClick={() => props.imageClick(props.id)} className="click"></span>
+    //   </div>
+      
+      <div onClick={() => props.imageClick(props.id)} className="card col-md-3">
+				<div className="img-container">
+					<img alt={props.name} src={props.image} />
+				</div>
+			</div>
     );
   }
   
